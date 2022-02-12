b99d66010ea708e019b4d365d0cf7886c725ea5f
MatyAlfonso <matt1998boca@gmail.com>
2022-02-12 11:22:09 +0100

    add display notes

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,15 +1,25 @@
 import { useEffect } from 'react';
 import { toast } from 'react-toastify';
 import { useSelector, useDispatch } from 'react-redux';
-import { getTicket, reset, closeTicket } from '../features/tickets/ticketSlice';
+import { getTicket, closeTicket } from '../features/tickets/ticketSlice';
+import { getNotes, reset as notesReset } from '../features/notes/noteSlice';
 import { useParams, useNavigate } from 'react-router-dom';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
-
+import NoteItem from '../components/NoteItem';
 
 const Ticket = () => {
 
-    const { ticket, isLoading, isSuccess, isError, message } = useSelector((state) => state.tickets);
+    const { ticket, isLoading, isSuccess, isError, message } =
+        useSelector(
+            (state) => state.tickets
+        );
+
+    const { notes, isLoading: notesIsLoading } =
+        useSelector(
+            (state) => state.notes
+        );
+
 
     const params = useParams();
     const navigate = useNavigate();
@@ -22,6 +32,7 @@ const Ticket = () => {
         }
 
         dispatch(getTicket(ticketId));
+        dispatch(getNotes(ticketId));
         // eslint-disable-next-line
     }, [isError, message, ticketId])
 
@@ -33,7 +44,7 @@ const Ticket = () => {
         navigate('/tickets');
     }
 
-    if (isLoading) {
+    if (isLoading || notesIsLoading) {
         return <Spinner />
     }
 
@@ -60,7 +71,11 @@ const Ticket = () => {
                     <h3>Description of issue</h3>
                     <p>{ticket.description}</p>
                 </div>
+                <h2>Notes</h2>
             </header>
+            {notes.map((note)=>(
+                <NoteItem key={note._id} note={note} />
+            ))}
             {ticket.status !== 'closed' && (
                 <button onClick={onTicketClose} className="btn btn-block btn-danger">Close Ticket</button>
             )}
